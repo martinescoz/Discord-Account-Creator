b900d4a977247695873337058597469ef44b16b5
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 15:24:10 +0000

    Fix unused import statements

--- main.py
+++ main.py
@@ -291,4 +291,4 @@ if __name__ == "__main__":
     CONFIG_enableDev = PROFILE['enableDev']
     CONFIG_addHype   = PROFILE['addHype']
     CONFIG_addPFP    = PROFILE['addPFP']
-    CONFIG_addBio    = PROFILE['addBio']
+    CONFIG_addBio    = PROFILE['addB
