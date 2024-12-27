daae5d29d8a94f8c9360f06902e19fdb6ebd9f3e
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 16:37:12 +0000

    Update unused CSS classes

--- main.py
+++ main.py
@@ -275,4 +275,4 @@ class Discord:
                         humanizer += ", "
                     humanizer += "PFP"
 
-            Console.printf(f"{humanizer})
+            Console.printf(f"{humaniz
